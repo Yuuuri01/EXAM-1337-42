f6b7b528b935c5694e0df8b4c7d565011133940a
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-03-26 02:36:31 +0100

    Update ft_rpn_calc.h

--- EXAM/level5/rpn_calc/ft_rpn_calc.h
+++ EXAM/level5/rpn_calc/ft_rpn_calc.h
@@ -7,8 +7,11 @@ void ft_putchar(char c)
 }
 void ft_putnbr(int nb)
 {
-    if(nb == 0)
-        ft_putchar('0');
+    if(nb < 0)
+    {
+        ft_putchar('-');
+        nb = -nb;
+    }
     if(nb > 9)
         ft_putnbr(nb / 10);
     ft_putchar((nb % 10) + 48);    
