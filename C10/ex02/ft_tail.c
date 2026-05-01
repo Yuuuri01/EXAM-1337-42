33ead6786dac0bccd2fc707b11f655a1d92a393a
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-05-01 20:50:33 +0200

    Add files via upload

---
+++ C10/ex02/ft_tail.c
@@ -0,0 +1,73 @@
+# include "lib_tail.h"
+# include <stdio.h>
+
+void ft_tail(char *file, char *program)
+{
+    int i;
+    int fd;
+    int total;
+    int start;
+    int bytes;
+    int count;
+    char *buff;
+    int size_buff;
+
+    // int size;
+
+    fd = open(file, O_RDONLY);
+    if(fd == -1)
+    {
+        ft_putstr(program);
+        write(2, ": ", 2);
+        ft_putstr("cannot open '");
+        ft_putstr(basename(file));
+        ft_putstr("' for reading: ");
+        ft_putstr(strerror(errno));
+        write(1, "\n", 1);
+        return;
+    }
+    total = 0;
+    size_buff = 30000;
+    buff = malloc(size_buff);
+    if(!buff)
+        return;
+
+    while ((bytes = read(fd, buff + total, 1)) > 0)
+    {
+        total += bytes;
+        if(total >= size_buff) // 31024
+        {
+            size_buff += 1024;
+            buff = malloc(size_buff);
+            if(!buff)
+            {
+                return;
+            }
+        }
+    }
+    count = 0;
+
+    //check last newline
+    if(total > 0 && buff[total - 1] == '\n')
+        total--;
+
+    //back to 10
+    i = total - 1;
+    while (i >= 0)
+    {
+        if (buff[i] == '\n')
+            count++;
+        if (count == 10)
+            break;
+        i--;
+    }
+
+    //start point   
+    if (i < 0)
+        start = 0;
+    else
+        start = i + 1;
+
+    write(1, buff + start, total - start);
+    free(buff);
+}
