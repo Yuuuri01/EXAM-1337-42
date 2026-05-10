d3915ea0db3eed7f59e7e81d066d462a7061c8a0
Yuuuuuri01 <hamzaofficer2@gmail.com>
2026-05-10 20:44:22 +0200

    Update ft_list_push_back.c

--- C12/ex04/ft_list_push_back.c
+++ C12/ex04/ft_list_push_back.c
@@ -1,5 +1,4 @@
 # include "ft_list.h"
-# include <stdio.h>
 
 
 t_list *ft_create_elm(void *data)
@@ -33,34 +32,4 @@ void ft_list_push_back(t_list **begin_list, void *data)
         node = node->next;
     }
     node->next = last;
-}
-void print_list(t_list *mlawi)
-{
-    while (mlawi)
-    {
-        printf("%d ", *(int *)mlawi->data);
-        mlawi = mlawi->next;
-    }
-    printf("\n");
-}
-int main()
-{
-    t_list *head;
-
-    t_list *node1 = malloc(sizeof(t_list));
-    t_list *node2 = malloc(sizeof(t_list));
-
-    int a = 100;
-    int b = 232;
-    int c = 4242;
-    head = node1;
-
-    node1->data = &a;
-    node1->next = node2;
-
-    node2->data = &b;
-    node2->next = NULL;
-
-    ft_list_push_back(&head, &c);
-    print_list(head);
 }
