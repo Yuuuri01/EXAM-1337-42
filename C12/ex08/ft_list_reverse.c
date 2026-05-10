6de0dfd8ff7c3aa104640a401388a274969225ed
Yuuuuuri01 <hamzaofficer2@gmail.com>
2026-05-10 20:42:50 +0200

    Update ft_list_reverse.c

--- C12/ex08/ft_list_reverse.c
+++ C12/ex08/ft_list_reverse.c
@@ -1,5 +1,4 @@
 # include "ft_list.h"
-# include <stdio.h>
 
 void ft_list_reverse(t_list **begin_list)
 {
@@ -18,38 +17,38 @@ void ft_list_reverse(t_list **begin_list)
     }
     *begin_list = prev;
 }
-void print_list(t_list *nd)
-{
-    while(nd)
-    {
-        printf("%d\n", *(int *)nd->data);
-        nd = nd->next;
-    }
-}
-int main()
-{
-    t_list *head;
+// void print_list(t_list *nd)
+// {
+//     while(nd)
+//     {
+//         printf("%d\n", *(int *)nd->data);
+//         nd = nd->next;
+//     }
+// }
+// int main()
+// {
+//     t_list *head;
 
-    t_list *node1 = malloc(sizeof(t_list));
-    t_list *node2 = malloc(sizeof(t_list));
-    t_list *node3 = malloc(sizeof(t_list));
+//     t_list *node1 = malloc(sizeof(t_list));
+//     t_list *node2 = malloc(sizeof(t_list));
+//     t_list *node3 = malloc(sizeof(t_list));
 
-    int a = 10;
-    int b = 20;
-    int c = 42;
+//     int a = 10;
+//     int b = 20;
+//     int c = 42;
 
-    node1->data = &a;
-    node1->next = node2;
+//     node1->data = &a;
+//     node1->next = node2;
 
-    node2->data = &b;
-    node2->next = node3;
+//     node2->data = &b;
+//     node2->next = node3;
 
-    node3->data = &c;
-    node3->next = NULL;
+//     node3->data = &c;
+//     node3->next = NULL;
 
-    head = node1;
-    print_list(head);
-    printf("--reverse----\n");
-    ft_list_reverse(&head);
-    print_list(head);
-}
+//     head = node1;
+//     print_list(head);
+//     printf("--reverse----\n");
+//     ft_list_reverse(&head);
+//     print_list(head);
+// }
