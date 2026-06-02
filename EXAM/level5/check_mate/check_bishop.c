d846250b754f7628771c7600bc5244e854f3f81f
Yuuuuuri01 <hamzaofficer2@gmail.com>
2026-06-02 12:10:54 -0400

    Update check_bishop.c

--- EXAM/level5/check_mate/check_bishop.c
+++ EXAM/level5/check_mate/check_bishop.c
@@ -1,52 +1,86 @@
+//trace
+//   2 [\] [.] [.] [/] 1
+//     [.] [2] [1] [.]
+//     [.] [1] [2] [.]
+//   3 [/] [.] [.] [\] 4
 int check_bishop(char **av, int row, int col)
 {
+    int i;
     int j;
-    int i = 0;
-    int block = 1;
-    int pass = 0;
-    char c = 0;
-    while(av[i] && block)
+    int access = 1;
+
+//see trace "/\" 
+//           /\
+//           /\
+
+    i = row - 1;
+    j = col + 1;
+    //top right  "/"  from [3 to 1]
+    while((i >= 0 && j >= 0) && (av[i] && av[i][j]))
     {
-        j = 0;
-        while(av[i][j])
-        {
-            if(block && (av[i][j] == 'P' || av[i][j] == 'Q' || av[i][j] == 'B' || av[i][j] == 'R' || av[i][j] == 'K'))
-            {
-                if((j - i == col - row) || (i + j == row + col))
-                {
-                    c = av[i][j];
-                    block = 0;
-                }
-            }
-            j++;
-        }
-        i++;
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.')
+            break;
+        i--;
+        j++;
     }
-    if(c == 'K')
-        pass = 1;
-    else if(c == 'B')
-        pass = 2;
-    i = 0;
-    while(av[i])
+    //down left "/" from [1 to 3]
+    i = row + 1;
+    j = col - 1;
+    while((i >= 0 && j >= 0) && (av[i] && av[i][j]))
     {
-        j = 0;
-        while(av[i][j])
-        {
-            if((av[i][j] == 'R' || av[i][j] == 'P') && (i + j == row + col || j - i == col - row))
-            {
-                if(c != 'R' && c != 'P')
-                    pass = 1;
-                else
-                    return 0;
-            }
-            else if((pass == 2 && av[i][j] == 'K') || (pass == 1 && av[i][j] == 'B'))
-            {
-                if((i + j == row + col) || (j - i == col - row))
-                    return 1;
-            }
-            j++;
-        }
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.')
+            break;
         i++;
+        j--;
+    }
+    //top left "\" from [4 to 2]
+    i = row - 1;
+    j = col - 1;
+    while((i >= 0 && j >= 0) && (av[i] && av[i][j]))
+    {
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.')
+            break;
+        i--;
+        j--;
+    }
+
+    //down right "\" from [2 to 4]
+    i = row + 1;
+    j = col + 1;
+    while((i >= 0 && j >= 0) && (av[i] && av[i][j]))
+    {
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.')
+            break;
+        i++;
+        j++;
     }
     return 0;
 }
+
+// int main()
+// {
+//     char *av[9];
+
+//     av[0] = "........";
+//     av[1] = "..B....."; 
+//     av[2] = ".....K..";
+//     av[3] = "....P...";
+//     av[4] = ".....K..";
+//     av[5] = "........";
+//     av[6] = "........";
+//     av[7] = "........";
+//     av[8] = NULL;
+
+//     if(check_bishop(av, 1, 2))
+//         printf("check\n");
+//     else
+//         printf("safe\n");
+// }
