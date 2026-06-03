09180e0a04f8381e7976a22b82f90e8c22bba0bd
Yuuuuuri01 <hamzaofficer2@gmail.com>
2026-06-02 21:50:40 -0400

    Update check_rock.c

--- EXAM/level5/check_mate/check_rock.c
+++ EXAM/level5/check_mate/check_rock.c
@@ -1,24 +1,51 @@
-
 int check_rock(char **av, int row, int col)
 {
-    if(col < 0 || row < 0)
-        return 0;
-
-    int i = 0;
+    int i;
     int j;
-    while(av[i])
+
+    //up row
+    i = row - 1;
+    j = col;
+    while(i >= 0 && av[i])
     {
-        j = 0;
-        while(av[i][j])
-        {
-            if(av[i][j] != '.' && ((i == row && j != col) || (i != row && j == col)))
-            {
-                if(av[i][j] == 'K')
-                    return 1;
-            }
-            j++;
-        }
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.' && av[i][j] != 'R')
+            break;
+        i--;
+    }
+    //row down
+    i = row + 1;
+    j = col;
+    while(i >= 0 && av[i])
+    {
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.' && av[i][j] != 'R')
+            break;
         i++;
+    }
+    //col left to right
+    i = row;
+    j = col + 1;
+    while(j >= 0 && av[i][j])
+    {
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.' && av[i][j] != 'R')
+            break;
+        j++;
+    }
+    //col right to left
+    i = row;
+    j = col - 1;
+    while(j >= 0 && av[i][j])
+    {
+        if(av[i][j] == 'K')
+            return 1;
+        else if(av[i][j] != '.' && av[i][j] != 'R')
+            break;
+        j--;
     }
     return 0;
 }
